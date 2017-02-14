8c42cecb930e7780d9b1981af4dc437e00253119
Ian Malerich <ian.malerich@gmail.com>
2017-02-14 00:51:42 +0000

    almost done with hw2, just a couple errors to find and fix

--- hw2/p1.R
+++ hw2/p1.R
@@ -1,5 +1,5 @@
 #########
-# Setup	#
+# Setup #
 #########
 
 library(foreach)
@@ -8,51 +8,44 @@ library(doParallel)
 data <- read.csv("magic04.data", header=F, sep=",")
 train.size <- 13000 # Given by homework specification
 
-######################
-# Develpoment Sample #
-######################
-# data <- data[sample(nrow(data)),] # Randomize the data set
-# data <- data[1:1000,]
-# train.size <- nrow(data) * 0.65
-
 #######
 # KNN #
 #######
  
-# # Try a bunch of different K-values,
-# err <- foreach (K=1:50, .combine = c) %do% {
-#     cl <- makeCluster(4)
-#     registerDoParallel(cl)
-# 
-#     # Run KNN 100 times for each K value.
-#     # Each run is independent, so we can speed things up a little
-#     # bit by running it in parallel.
-#     k.err <- foreach (i=1:100, .combine = c) %dopar% {
-# 	# Need to load the library for knn on each thread.
-# 	library(class)
-# 
-# 	data <- data[sample(nrow(data)),] # Randomize the data set
-# 
-# 	train <- data[1:train.size, 1:10]
-# 	test <- data[(train.size+1):nrow(data), 1:10]
-# 	train.cl <- factor(data[1:train.size, 11])
-# 	test.cl <- factor(data[(train.size+1):nrow(data), 11]);
-# 
-# 	predict.cl <- knn(train, test, train.cl, k=K)
-# 	sum(test.cl != predict.cl) / nrow(test)
-#     }
-# 
-#     stopCluster(cl)
-#     mean(k.err)
-# }
-# 
-# # This was our best performing k value.
-# k <- which.min(err)
-# min.err <- min(err)
-# acc <- 1.0 - min.err
-# 
-# # About 80.975%
-# print(paste("KNN - Accuracy: ", acc))
+# Try a bunch of different K-values,
+err <- foreach (K=1:50, .combine = c) %do% {
+    cl <- makeCluster(4)
+    registerDoParallel(cl)
+
+    # Run KNN 100 times for each K value.
+    # Each run is independent, so we can speed things up a little
+    # bit by running it in parallel.
+    k.err <- foreach (i=1:100, .combine = c) %dopar% {
+	# Need to load the library for knn on each thread.
+	library(class)
+
+	data <- data[sample(nrow(data)),] # Randomize the data set
+
+	train <- data[1:train.size, 1:10]
+	test <- data[(train.size+1):nrow(data), 1:10]
+	train.cl <- factor(data[1:train.size, 11])
+	test.cl <- factor(data[(train.size+1):nrow(data), 11]);
+
+	predict.cl <- knn(train, test, train.cl, k=K)
+	sum(test.cl != predict.cl) / nrow(test)
+    }
+
+    stopCluster(cl)
+    mean(k.err)
+}
+
+# This was our best performing k value.
+k <- which.min(err)
+min.err <- min(err)
+acc <- 1.0 - min.err
+
+# About 80.975%
+print(paste("KNN - Accuracy: ", acc))
  
 #######
 # LDA #
@@ -114,31 +107,56 @@ train.size <- 13000 # Given by homework specification
 # Naive Bayes (Normal) #
 ########################
 
-cl <- makeCluster(4)
-registerDoParallel(cl)
-
-err <- foreach (i=1:100, .combine = c) %dopar% {
-    library(klaR)
-    library(caret)
-
-    data <- data[sample(nrow(data)),] # Randomize the data set
-
-    train <- data[1:train.size, 1:10]
-    test <- data[(train.size+1):nrow(data), 1:10]
-    train.cl <- factor(data[1:train.size, 11])
-    test.cl <- factor(data[(train.size+1):nrow(data), 11]);
-
-    model <- train(train, train.cl, 'nb')
-    predict(model)
-    predict.cl <- predict(model$finalModel, test)$class
-    sum(test.cl != predict.cl) / nrow(test)
-}
-
-stopCluster(cl)
-acc <- 1.0 - mean(err)
-
-print(paste("Naive Bayes - Accuracy: ", acc))
+# cl <- makeCluster(4)
+# registerDoParallel(cl)
+# 
+# err <- foreach (i=1:100, .combine = c) %dopar% {
+#     library(klaR)
+#     library(caret)
+# 
+#     data <- data[sample(nrow(data)),] # Randomize the data set
+# 
+#     train <- data[1:train.size, 1:10]
+#     test <- data[(train.size+1):nrow(data), 1:10]
+#     train.cl <- factor(data[1:train.size, 11])
+#     test.cl <- factor(data[(train.size+1):nrow(data), 11]);
+# 
+#     model <- NaiveBayes(x = train, grouping = train.cl, usekernel=FALSE)
+#     predict.cl <- predict(model, test)$class
+#     sum(test.cl != predict.cl) / nrow(test)
+# }
+# 
+# stopCluster(cl)
+# acc <- 1.0 - mean(err)
+# 
+# # About 72.6714%
+# print(paste("Naive Bayes (Normal) - Accuracy: ", acc))
 
 ########################
 # Naive Bayes (Kernel) #
 ########################
+
+# cl <- makeCluster(4)
+# registerDoParallel(cl)
+# 
+# err <- foreach (i=1:100, .combine = c) %dopar% {
+#     library(klaR)
+#     library(caret)
+# 
+#     data <- data[sample(nrow(data)),] # Randomize the data set
+# 
+#     train <- data[1:train.size, 1:10]
+#     test <- data[(train.size+1):nrow(data), 1:10]
+#     train.cl <- factor(data[1:train.size, 11])
+#     test.cl <- factor(data[(train.size+1):nrow(data), 11]);
+# 
+#     model <- NaiveBayes(x = train, grouping = train.cl, usekernel=TRUE)
+#     predict.cl <- predict(model, test)$class
+#     sum(test.cl != predict.cl) / nrow(test)
+# }
+# 
+# stopCluster(cl)
+# acc <- 1.0 - mean(err)
+# 
+# # About 76.2375%
+# print(paste("Naive Bayes (Kernel) - Accuracy: ", acc))
--- hw2/p2.R
+++ hw2/p2.R
@@ -1,27 +1,47 @@
 #########
-# Setup	#
+# Setup #
 #########
 
 library(MASS)
+library(klaR)
 
 data <- data.frame(
-    c(0, 1, 1, 1, 0, 1),
-    c(0, 0, 0, 1, 1, 1),
-    c(0, 1, 0, 1, 1 ,0),
-    c(0, 1, 0, 1, 1 ,0)
+    c(0.6585, 2.2460, -2.7665, -1.2565, -0.7973, 1.1170),
+    c(0.2444, 0.5281, -3.8303, 3.4912, 1.2288, 2.2637),
+    c(0, 0, 0, 1, 1, 1)
 )
 
-names(data) <- c("F1", "F2", "F3", "CLASS")
-test <- c(0, 0, 1)
+test <- data.frame(c(0), c(1))
+
+names(data) <- c("F1", "F2", "CLASS")
+names(test) <- c("F1", "F2")
+
+train.size <- nrow(data)
+train <- data[1:train.size, 1:2]
+train.cl <- factor(data[1:train.size, 3])
  
 #######
 # QDA #
 #######
 
-train.size <- nrow(data)
-train <- data[1:train.size, 1:3]
-train.cl <- factor(data[1:train.size, 4])
-
 model <- qda(x = train, grouping = train.cl)
-predict.cl <- predict(model, test)$class
-print(predict.cl)
+predict <- predict(model, test)
+print(predict)
+print(paste("QDA: ", predict$class))
+
+# Posterior Class Probabilities
+# 0: 0.0067895
+# 1: 0.9932105
+
+###############
+# Naive Bayes #
+###############
+
+model <- NaiveBayes(x = train, grouping = train.cl, usekernel=FALSE)
+predict <- predict(model, test)
+print(predict) 
+print(paste("Naive Bayes: ", predict$class))
+
+# Posterior Class Probabilities
+# 0: 0.2493135
+# 1: 0.7506865
